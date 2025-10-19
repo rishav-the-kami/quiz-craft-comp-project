43286eada889a8e4ec6e5bd64b61b3da46dadc93
Rishav <105970051+rishav-the-kami@users.noreply.github.com>
2025-10-19 22:34:05 +0530

    Update quiztaker.py

--- quiztaker.py
+++ quiztaker.py
@@ -85,54 +85,8 @@ try:
                 score_label = tk.Label(self.window, text="Your score is: " + (str(self.score+1)))
                 score_label.pack()
 
-            
-            # print("in quiztaker:", self.user_id, self.name, self.quiz_id)
-            
-            # self.option_entries = []
-            # for i in range(4):
-            #     label = tk.Label(self.window, text=f"Option {i+1}:")
-            #     label.pack()
-            #     entry = tk.Entry(self.window)
-            #     entry.pack()
-            #     self.option_entries.append(entry)
-            
-            # self.correct_var = tk.IntVar()
-            # self.correct_label = tk.Label(self.window, text="Correct option (1-4):")
-            # self.correct_label.pack()
-            # self.correct_entry = tk.Entry(self.window)
-            # self.correct_entry.pack()
-            
-            # self.next_button = tk.Button(self.window, text="Next", command=self.next_question)
-            # self.next_button.pack()
-
-        
-
-        # def next_question(self):
-        #     question = self.question_entry.get()
-        #     options = [entry.get() for entry in self.option_entries]
-        #     correct = int(self.correct_entry.get()) - 1
-            
-        #     self.questions.append((question,str(options),correct))
-
-        #     mycursor.execute("INSERT INTO quizzes(qno, question, options, correct, created_by, id) VALUES (%s, %s,%s,%s, %s, %s)", (self.current_question, question, str(options),correct, self.name, self.no_of_quizzes))
-        #     print("Added Question", self.current_question)
-            
-        #     self.current_question += 1
-            
-        #     if self.current_question < 4:
-        #         self.question_label.config(text=f"Question {self.current_question + 1}:")
-        #         self.question_entry.delete(0, tk.END)
-        #         for entry in self.option_entries:
-        #             entry.delete(0, tk.END)
-        #         self.correct_entry.delete(0, tk.END)
-        #     else:
-        #         print("Quiz creation complete!")
-        #         print(self.questions)
-        #         mydb.commit()
-
-        #         self.window.withdraw()
-        #         Homepage.Homepagewindow(self.name, self.user_id)
                 
 except Exception as e:
 
     print(e)
+
